2f313d6ecb00de354185534c199d951fd2e7158b
Olha Chumbash <olyachumbash@gmail.com>
2025-09-12 18:23:05 +0200

    add basic elementa and meta tags

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import '../App.css';
+
+
+function Nav() {
+    return (
+        <div className="Nav">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About us</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Nav;
