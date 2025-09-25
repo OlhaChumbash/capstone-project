24a59950ccca6f88d34a70df25f2d90e8a1e4f65
Olha Chumbash <olyachumbash@gmail.com>
2025-09-25 23:56:17 +0200

    add tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './components/HomePage';
 import BookingPage  from './components/BookingPage';
+import HomePage from './components/HomePage';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
